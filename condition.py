904c270be1391c99e261cf1b676f89f09eb94c77
changxianglin <281532612@qq.com>
2020-02-27 00:32:06 +0800

    condition

---
+++ condition.py
@@ -0,0 +1,19 @@
+is_hot = False
+is_cold = True
+
+if is_hot:
+    print("It's a hot day")
+    print("Drink plenty of water")
+elif is_cold:
+    print("It's a cold day")
+    print("Wear warm clothes")
+else:
+    print("It's a lovely day")
+print("Enjoy you day")
+
+
+has_high_income = True
+has_good_credit = True
+
+if has_high_income and has_good_credit:
+    print("Eligible for loan")
