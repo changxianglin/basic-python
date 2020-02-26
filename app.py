f36e47d131cec5867f1fabac133e9b3dd685d0a2
changxianglin <281532612@qq.com>
2020-02-26 23:47:50 +0800

    basic

---
+++ app.py
@@ -0,0 +1,3 @@
+name = input('What is your name? ')
+favorite_color = input('What is you favorite color?')
+print(name + ' likes ' + favorite_color)
---
+++ course.py
@@ -0,0 +1,13 @@
+course = "Python's for Beginners"
+print(course)
+
+task = '''
+Python is very
+
+strong language 
+
+
+code
+'''
+
+print(task)
